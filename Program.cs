1ab17730798ac329ae646d25b5d82edff783296a
Ayman <enkhalifapro@gmail.com>
2020-04-08 00:00:22 +0200

    static samples

--- Program.cs
+++ Program.cs
@@ -9,10 +9,22 @@ namespace blog
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Aia from Pradise!");
-            int x = 10;
 
-            AdminUser u = new AdminUser();
-            
+            // TypeName VarName = value;
+            Console.WriteLine(Post.Fake); // ""
+            Post pst = new Post(); // referencType
+            pst.UpdateFake();
+            Console.WriteLine(Post.Fake); // 11
+            Console.WriteLine(pst.Real); // 11
+            Post.Fake = 20;
+            Post pst2 = new Post();
+            Console.WriteLine(Post.Fake); // "10"
+            Console.WriteLine(pst2.Real); // 10
+
+            pst.UpdateFake(); //
+            Console.WriteLine(Post.Fake); // "11"
+            Console.WriteLine(pst2.Real); // 10
+
         }
     }
 }
--- models/Post.cs
+++ models/Post.cs
@@ -5,6 +5,31 @@ namespace blog.models
 {
     public class Post : BaseModel
     {
+        public static int Fake { get; set; }
+        public int Real { get; set; }
+
+        // behaviour 
+        // Constructor
+        public Post()
+        {
+            Fake = 10;
+            Real = 10;
+            Title = "Hello";
+        }
+
+        // function
+        public int UpdateFake()
+        {
+            Fake++;
+            Real++;
+            return Fake;
+        }
+
+        // States
+        // attribute: very weak member that lives in class and shiould be private
+        public string title;
+
+        // Property: stronger and secured version of attribute, by which we can prevent primitive obssetion 
         public string Title { get; set; }
         public string Content { get; set; }
         public List<Like> Likes { get; set; }
@@ -14,5 +39,19 @@ namespace blog.models
         public bool IsPublished { get; set; }
         public User Author { get; set; }
         public DateTime PublishDate { get; set; }
+    }
+
+    public static class PostUtil
+    {
+        static PostUtil()
+        {
+        }
+
+        public static int PostCount { get; set; }
+
+        public static int GetPostCount()
+        {
+            return 10;
+        }
     }
 }
