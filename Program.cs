3d8086c4e5b6f72419239626f6c17956fe7d5f72
Ayman <enkhalifapro@gmail.com>
2020-04-13 07:45:44 +0200

    add latest code

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Linq;
+using blog.DAL;
 using blog.memoryDB;
 using blog.models;
 using blog.services;
@@ -19,60 +20,10 @@ namespace blog
         {
             Console.WriteLine("Hello Aia from Pradise!");
 
-            string text = File.ReadAllText("Users.json");
-            Console.WriteLine("Contents of User.json = {0}", text);
-            Post pst = new Post(); //int x=10 calling the constructor
-            // NormalUserService userSrv = new NormalUserService();
-            // for (int i = 0; i < 5; i++)
-            // {
-            //     userSrv.SignUp(new NormalUser()
-            //     {
-            //         Email = i + "test@mail.com",
-            //         Name = "test" + i,
-            //         Password = "pass"
-            //     });
-            Postutil.getPostCount();
-
-            readFromFile("Users.txt");
-
-            // }
-
-
-            // saveToFile(memoryDB.MemoryDB.NormalUsers,"try2.txt");
-
-
-            /*// login
-            bool logged = userSrv.Login("0test@mail.com", "pass");
-            if (logged)
-            {
-                Console.WriteLine("First login has been done succesfully");
-            }
-
-
-            userSrv.Block("0test@mail.com", "1test@mail.com");
-
-            if (MemoryDB.NormalUsers.First(u => u.Email == "0test@mail.com").BlockedUsers
-                .Exists(s => s == "1test@mail.com"))
-            {
-                Console.WriteLine("User has been blocked successfully");
-            }*/
+            // Login
+            IRepository<NormalUser> normalUserRepo=new 
+            NormalUserService userSrv =new NormalUserService();
         }
-
-        // static void saveToFile(object data, string fileName)
-        // {
-        //         // var data = memoryDB.MemoryDB.NormalUsers;
-        //         var DatasString = JsonConvert.SerializeObject(data);
-        //         // Console.WriteLine(DatasString);
-        //         // string filePath = @"Users.json";
-        //         using (StreamWriter outputFile = new StreamWriter(fileName))
-        //         {
-        //             outputFile.WriteLine(DatasString);
-        //         }
-
-        //         Console.WriteLine("###Saving data to "+fileName+" is done successfully ###");
-
-
-        // }
 
         //read from file
         static void readFromFile(string fileName)
