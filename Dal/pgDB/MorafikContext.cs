81e4b5476a6c8616363ce73a7872ae4a6c135d3c
Ayman <enkhalifapro@gmail.com>
2020-05-10 06:09:22 +0200

    add bll

--- Dal/pgDB/MorafikContext.cs
+++ Dal/pgDB/MorafikContext.cs
@@ -14,5 +14,18 @@ namespace Dal.pgDB
         public DbSet<User> Users { get; set; }
         public DbSet<Sighted> Sighteds { get; set; }
         public DbSet<Blind> Blinds { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceTime> ServiceTimes { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+        public DbSet<SightedLanguage> SightedLanguages { get; set; }
+        public DbSet<SightedService> SightedServices { get; set; }
+        public DbSet<SightedSkill> SightedSkills { get; set; }
     }
 }
